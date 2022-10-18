42332a4496f17eb24229b68f69dee8e50f3762d8
Harry Vince <47283812+harryvince@users.noreply.github.com>
2022-10-18 22:36:23 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 # Setup packages required for script
 echo 'Setting up tools...'
 packagesNeeded='curl jq tmux vim htop'
-if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $packagesNeeded
-elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install $packagesNeeded
-elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
-elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
-elif [ -x "$(command -v yum)" ]; then sudo yum install $packagesNeeded
+if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $packagesNeeded -y
+elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install $packagesNeeded -y
+elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded -y
+elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded -y
+elif [ -x "$(command -v yum)" ]; then sudo yum install $packagesNeeded -y
 else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
 cat > ~/.tmux.conf <<'EOE'
 # remap prefix from 'C-b' to 'C-a'
