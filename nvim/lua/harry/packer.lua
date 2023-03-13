40b76cacca24f8e96c29ce9311dccf69349ff862
Harry Vince <harryavince@gmail.com>
2023-03-13 18:49:19 +0000

    adding latest dotfiles

--- nvim/lua/harry/packer.lua
+++ nvim/lua/harry/packer.lua
@@ -26,7 +26,7 @@ return require('packer').startup(function(use)
         'olivercederborg/poimandres.nvim',
         config = function()
             require('poimandres').setup {
-                disable_nc_background = true,
+                dim_nc_background = true,
                 disable_background = true,
                 disable_float_background = false,
             }
@@ -49,12 +49,12 @@ return require('packer').startup(function(use)
             { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },         -- Required
-            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
-            { 'hrsh7th/cmp-buffer' },       -- Optional
-            { 'hrsh7th/cmp-path' },         -- Optional
-            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-            { 'hrsh7th/cmp-nvim-lua' },     -- Optional
+            { 'hrsh7th/nvim-cmp' },          -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },      -- Required
+            { 'hrsh7th/cmp-buffer' },        -- Optional
+            { 'hrsh7th/cmp-path' },          -- Optional
+            { 'saadparwaiz1/cmp_luasnip' },  -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },      -- Optional
 
             -- Snippets
             { 'L3MON4D3/LuaSnip' },             -- Required
