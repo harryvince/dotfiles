b450ed3e624d7a939432fb8d6a27aece82b9e4c2
Harry Vince <harryavince@gmail.com>
2023-03-16 18:30:48 +0000

    adding better support

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if ! command -v <nvim> &> /dev/null
+then
+    echo "Neovim must be installed before setup"
+    exit
+fi
+
 echo "Installing Packer..."
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
@@ -9,3 +15,4 @@ mkdir $HOME/.config/nvim/plugin
 nvim +PackerSync
 
 echo "Script Finished."
+echo "Make sure to install lazygit & ripgrep for the neovim setup to run"
