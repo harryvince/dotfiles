92e3a06c5efb757705dd23c6b098b01becf67346
Harry Vince <harryavince@gmail.com>
2023-03-17 20:19:00 +0000

    fixing telescope + adding treesitter context

--- nvim/lua/harry/packer.lua
+++ nvim/lua/harry/packer.lua
@@ -34,6 +34,7 @@ return require('packer').startup(function(use)
     }
 
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use("nvim-treesitter/nvim-treesitter-context")
     use('theprimeagen/harpoon')
     use('mbbill/undotree')
     use('voldikss/vim-floaterm')
