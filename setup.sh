8b8de061fd2eeb026009e6fd6a4ab8ffab645dbf
Harry Vince <harryavince@gmail.com>
2023-03-17 20:02:48 +0000

    updating setup_script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
-# Setup packages required for script
+# Setup base packages required for script
 echo 'Setting up tools...'
-packagesNeeded='curl jq tmux vim htop git'
-if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $packagesNeeded -y
-elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install $packagesNeeded -y
-elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded -y
-elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded -y
-elif [ -x "$(command -v yum)" ]; then sudo yum install $packagesNeeded -y
-else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-echo y | ~/.fzf/install
-echo 'Tool Setup Finished'
-echo 'Please ensure to source your rc file for fzf to work correctly'
+packagesNeeded='curl jq tmux vim htop git ripgrep unzip'
+if [ -x "$(command -v apt-get)" ]
+then
+    sudo apt-get install $packagesNeeded -y
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit /usr/local/bin
+    sudo apt-get install zsh -y
+    wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
+    sudo apt install ./nvim-linux-64.deb
+    rm nvim-linux64.deb
+elif [ -x "$(command -v brew)" ]
+then
+    brew install $packagesNeeded
+    brew install jesseduffield/lazygit/lazygit
+    brew install neovim
+else
+    echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
+fi
+
+# Setup oh my zsh
+echo "Installing oh my zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp .zshrc ~/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Neovim
+cp -r nvim ~/.config/nvim
+echo "Installing Packer..."
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+echo "Running Packer..."
+mkdir $HOME/.config/nvim/plugin
+nvim +PackerSync
+
+echo "Script Finished."
+echo "Make sure to install lazygit & ripgrep for the neovim setup to run"
