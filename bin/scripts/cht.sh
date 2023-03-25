4816762bbdef68cb2b930a1fb4b73012433203d7
Harry Vince <harryavince@gmail.com>
2023-03-25 23:04:46 +0000

    fixed cht.sh

--- bin/scripts/cht.sh
+++ bin/scripts/cht.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
-selected=`cat ~/bin/scripts/cht/languages ~/bin/scripts/cht/commands  | fzf`
+selected=`cat ~/bin/scripts/cht/list | fzf`
 if [[ -z $selected ]]; then
     exit 0
 fi
 
 read -p "Enter query: " query
 
-if grep -qs "$selected" ~/bin/scripts/cht/languages; then
-    query=`echo $query | tr ' ' '+'`
-    curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done
-else
-    curl -s cht.sh/$selected~$query | less
-fi
+query=`echo $query | tr ' ' '+'`
+curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done
