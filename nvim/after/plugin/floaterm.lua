cc2112b47b2ac4106237d53c6c3e0a802732e99f
Harry Vince <47283812+harryvince@users.noreply.github.com>
2023-04-09 02:11:04 +0100

    Update floaterm.lua

--- nvim/after/plugin/floaterm.lua
+++ nvim/after/plugin/floaterm.lua
@@ -2,5 +2,5 @@ local options = { noremap = true }
 
 local dir = vim.fn.expand('ls %:p:h')
 
-    vim.keymap.set('n', '<leader>tt', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 zsh<CR>', options)
+vim.keymap.set('n', '<leader>tt', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 zsh<CR>', options)
 vim.keymap.set('n', '<leader>ti', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 ~/bin/scripts/cht.sh<CR>', options)
