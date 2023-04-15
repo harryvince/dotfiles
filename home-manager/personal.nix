f44694ba99765f428e7b79b7b59273ce7f0f6b46
Harry Vince <harryavince@gmail.com>
2023-04-15 17:49:55 +0100

    fix for private config files

--- home-manager/personal.nix
+++ home-manager/personal.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 let
     shared_config = import ./shared.nix { inherit pkgs config; };
-    tmux_conf = pkgs.fetchgit {
+    private_config = pkgs.fetchgit {
         rev = "master";
         sha256 = "1ds251q4s6s0y3hkmrvkgr9al0fzqh2ydl5v0zl3sic0gc2gzmah";
         url = "git@github.com:harryvince/personal.git";
@@ -9,7 +9,7 @@ let
 in
 {
     home.file."background".source = ../2560x1440-Wallpaper-Free-Download-1.jpg;
-    home.file."personal".source = tmux_conf;
+    home.file.".tmux-personal.conf".source = private_config + /.configs/.tmux-personal.conf;
     home.packages = import ./packages/personal.nix { inherit pkgs; };
     imports = [ shared_config ];
 }
