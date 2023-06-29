7369839b4037be03acc3e2f8535d37e316f8d877
Harry Vince <harryavince@gmail.com>
2023-06-29 22:38:09 +0100

    adding darwin config??

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -7,16 +7,18 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    darwin = {
+        url = "github:lnl7/nix-darwin";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs = inputs@{ flake-parts, darwin, ... }:
     let
       system = "x86_64-linux";
       pkgs = inputs.nixpkgs.legacyPackages.${system};
       home-manager = inputs.home-manager;
     in {
-      home-manager.useUserPackages = true;
-      home-manager.useGlobalPkgs = true;
       homeConfigurations.shared = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [ ./shared.nix ];
@@ -24,6 +26,17 @@
       homeConfigurations.personal = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [ ./personal.nix ];
+      };
+      homeConfigurations.darwin = darwin.lib.darwinSystem {
+          system = "aarch64-darwin";
+          modules = [
+            home-manager.darwinModules.home-manager
+            {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.harryvince = import ./shared.nix;
+            }
+          ];
       };
     };
 }
