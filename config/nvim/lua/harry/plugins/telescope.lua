ccc4b2910ca697b528d01e2d99f7827e722105ad
Harry Vince <harryavince@gmail.com>
2023-07-02 22:59:07 +0100

    updating telescope and integrating noice

--- config/nvim/lua/harry/plugins/telescope.lua
+++ config/nvim/lua/harry/plugins/telescope.lua
@@ -5,9 +5,14 @@ return {
     dependencies = { { 'nvim-lua/plenary.nvim' }, { 'kdheepak/lazygit.nvim' } },
     config = function()
         require('telescope').load_extension('lazygit')
+        require('telescope').load_extension('notify')
         local builtin = require('telescope.builtin')
+        -- File finding 
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>bf', builtin.buffers, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+        -- LSP Helpers
+        vim.keymap.set('n', '<leader>se', builtin.diagnostics, {})
     end,
 }
