caece2d92b74ba02d4d9be5851d83dca2e71ccbc
Harry Vince <harryavince@gmail.com>
2023-09-14 11:15:10 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 mac=${1}
 
+git_username="Harry Vince"
+git_email="harryavince@gmail.com"
 system_packages=('ansible' 'awscli2' 'zoxide' 'bat' 'fd' 'htop'
     'jq' 'just' 'ripgrep' 'unzip' 'wget' 'xclip' 'lazygit' 'tmux' 'neovim')
 package_string=""
@@ -27,20 +29,33 @@ setup_homebrew() {
 }
 
 linux_base() {
+    echo "Installing ZSH and setting it to the default terminal."
     brew install zsh
     chsh -s $(which zsh)
 }
 
 setup_ohmyzsh() {
+    echo "Setting up ohmyzsh."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
+check_then_symlink() {
+    location=${1}
+    dir_to_link=${2}
+    if [ -e "$location" ]; then
+        cp -r $location "$link_destination.backup"
+    fi
+    ln -s $dir_to_link $location
 }
 
 setup_system() {
     concat_packages
     brew install $package_string
-    git config --global user.name "Harry Vince"
-    ln -s config/nvim ~/.config/nvim
-    ln -s config/.tmux.conf ~/.tmux.conf
+    echo "Setting git config."
+    git config --global user.name "$git_username"
+    git config --global user.email "$git_email"
+    check_then_symlink ~/.config/nvim config/nvim
+    check_then_symlink ~/.tmux.conf config/.tmux.conf
 }
 
 echo "Just prompting for sudo to enable a seamless setup."
