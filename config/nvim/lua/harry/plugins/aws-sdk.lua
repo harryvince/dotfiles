185625a076b47a71cf23200fe5077bbe552da879
Harry Vince <harryavince@gmail.com>
2023-11-15 21:37:56 +0000

    adding my new plugin :)

---
+++ config/nvim/lua/harry/plugins/aws-sdk.lua
@@ -0,0 +1,16 @@
+return {
+    "harryvince/aws-sdk.nvim",
+    dependencies = {
+        { 'nvim-telescope/telescope.nvim' },
+        { 'nvim-lua/plenary.nvim' }
+    },
+    name = "aws-sdk",
+    dev = false,
+    event = 'VeryLazy',
+    config = function ()
+        local options = { noremap = true }
+        local aws = require('aws-sdk')
+
+        vim.keymap.set('n', '<leader>aws', aws.find_command)
+    end
+}
